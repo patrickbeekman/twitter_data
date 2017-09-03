fab201e0dfb315766e56067c7a9182a515bab41a
patrickbeekman <patrickbeekman1095@gmail.com>
2017-09-03 15:08:16 -0400

    Removed my keys.

--- MyTweets/tweet_parser.py
+++ MyTweets/tweet_parser.py
@@ -4,10 +4,10 @@ import json
 
 def main(args=None):
     # The main routine
-    consumer_key = 'e0tEUlJW8x1fAJpRSSHk7ynRf'
-    consumer_secret = 'GPhS5h6ylPede6EPnui0M3pODZgznohbCDOqQRFHYjFGwk0AX5'
-    access_token = '319943657-R90Uu0oUZ9Twf4Gmv7K5auqWHzr2p7QGb8yPHOAF'
-    access_secret = 'M4zl9SkOkma1iLLTpSzahpx62l6tvGiaTynmEREGE8I3g'
+    consumer_key = ''
+    consumer_secret = ''
+    access_token = ''
+    access_secret = ''
      
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
