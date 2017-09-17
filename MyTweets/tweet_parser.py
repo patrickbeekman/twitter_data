8b91e0c8e54a4ffc44053e4509df946bb0a81c7e
patrickbeekman <patrickbeekman1095@gmail.com>
2017-09-17 14:59:10 -0400

    Now writes the json for my tweets to a file, next step is to interpret the information from the .json file.

--- MyTweets/tweet_parser.py
+++ MyTweets/tweet_parser.py
@@ -1,23 +1,35 @@
 import tweepy
 from tweepy import OAuthHandler
 import json
+import pandas as pd
+import numpy as np
 
 def main(args=None):
     # The main routine
-    consumer_key = ''
-    consumer_secret = ''
-    access_token = ''
-    access_secret = ''
+    keys = open('keys.txt', 'r')
+    keys.readline() #skip the first line of comments
+    consumer_key = keys.readline().rstrip()
+    consumer_secret = keys.readline().rstrip()
+    access_token = keys.readline().rstrip()
+    access_secret = keys.readline().rstrip()
      
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
-      
+
     api = tweepy.API(auth)
 
-    for tweet in tweepy.Cursor(api.user_timeline).items(10):
-        print(json.dumps(tweet._json))
-    
+    output_file = open('tweets.json', 'w')
+    mylist = []
 
+    for tweet in tweepy.Cursor(api.user_timeline).items(10):
+        mylist.append(tweet._json)
+    
+    process_and_write(mylist, output_file)
+
+def process_and_write(tweets, out_file):
+    for item in tweets:
+        out_file.write(json.dumps(item) + "\n")
+    #out_file.write(json.dumps(tweet))
 
 if __name__ == "__main__":
     main()
